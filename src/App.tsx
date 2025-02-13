4e6e1298d3fe396ed91a3dcd30a56646f5e2a215
kinako1415 <Owata09@outlook.jp>
2025-02-13 10:25:32 +0900

    ロード中かの判断

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAllPokemon } from "./utils/pokemon.ts";
 import "./App.css";
 
 export function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       //全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
   });
 
-  return <></>;
+  return (
+    <>
+      {loading ? <h1>ロード中...</h1> : <h1>ポケモンデータを取得しました。</h1>}
+    </>
+  );
 }
